f0bab0039184b7c0090b5b00074fec334001f36a
ineswilms <i.wilms@maastrichtuniversity.nl>
2026-05-25 15:30:30 +0200

    session1 update

---
+++ solutions/S1.R
@@ -0,0 +1,10 @@
+# Solutions Session 1
+
+# Exercise 1.7
+library('rio')
+import("data/climate_wide.dta")
+import("data/climate.csv")
+
+# Exercise 1.8
+save.image() # this saves your current workspace
+save(AAPL, "data/AAPL.RData") # saving a specific object
